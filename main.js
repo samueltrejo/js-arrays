d707d1e590c30eca51c1b462654d0d90aad1e3b9
Samuel Trejo <samuel.trejo@s.tcatnashville.edu>
2019-03-05 20:24:53 -0600

    print to dom and style

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 const dinosaurs = [
     {
-        dinoType: 't-rex'
+        dinoType: 't-rex',
+        name: 'wrex'
     },
     {
-        dinoType: 'stegosaurus'
+        dinoType: 'stegosaurus',
+        name: 'steve'
     },
     {
-        dinoType: 'velociraptor'
+        dinoType: 'velociraptor',
+        name: 'ted'
     },
 ];
 
 //loop over the dinosaurs
 //build up a dom string
 //print dom srting to the dom
+
 const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
@@ -21,7 +25,10 @@ const printToDom = (divId, textToPrint) => {
 const buildDinosaurs = () => {
     let domString = '';
     for(let i = 0; i < dinosaurs.length; i++) {
+        domString += `<div class="dinosaur">`
         domString += `<h3>${dinosaurs[i].dinoType}</h3>`
+        domString += `<p>${dinosaurs[i].name}`
+        domString += `</div>`
     }
     printToDom('dino-barn', domString);
 };

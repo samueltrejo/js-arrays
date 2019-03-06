a8583befebafea3e5ce335616d45dfbdc67b736e
Samuel Trejo <samuel.trejo@s.tcatnashville.edu>
2019-03-05 20:05:27 -0600

    add builddino, printtodom, and init fucntions

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log('hey');
+const dinosaurs = [
+    {
+        dinoType: 't-rex'
+    },
+    {
+        dinoType: 'stegosaurus'
+    },
+    {
+        dinoType: 'velociraptor'
+    },
+];
+
+//loop over the dinosaurs
+//build up a dom string
+//print dom srting to the dom
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+}
+
+const buildDinosaurs = () => {
+    let domString = '';
+    for(let i = 0; i < dinosaurs.length; i++) {
+        domString += `<h3>${dinosaurs[i].dinoType}</h3>`
+    }
+    printToDom('dino-barn', domString);
+};
+
+const init = () => {
+    buildDinosaurs();
+};
+
+init();
